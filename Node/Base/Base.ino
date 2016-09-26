95be12e59c1f7775c7d3aa01d8957de3d557c1b8
Johannes Jäger <johannes.jaeger@lexmark.com>
2016-09-26 21:50:50 +0200

    added WLANConf

---
+++ Node/Base/Base.ino
@@ -0,0 +1,16 @@
+#include "WLAN_CONF.h"
+
+
+
+WLAN_CONF conf;
+void setup() {
+  // put your setup code here, to run once:
+  //conf.foo(ssid);
+  conf.SOMETHING;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
