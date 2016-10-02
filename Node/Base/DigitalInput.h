3f124ac498caa2acc5c64c1fa0f5ed29888d8fae
Johannes Jäger <johannes.jaeger@lexmark.com>
2016-10-02 22:33:10 +0200

    refined Timer; continued on DigitalInput

--- Node/Base/DigitalInput.h
+++ Node/Base/DigitalInput.h
@@ -1,34 +1,88 @@
-//#include "Node_Base.h"
+#include "TimeElement.h"
 
 class DigitalInput {
-    public:
-        DigitalInput(int id, 
-                     int parentId, 
-                     int portNoOnParentHardware, 
-                     bool reverseValue,
-                     int debounceTimeMillis);
-        void tick();
-        void setInputValueFromHardware(bool value);
-        bool currentValue();
-    private:
-        int id;
-        int parentId;
-        int portNumber;
-        bool reverseOutput;
-        bool currentHardwareValue;
+public:
+	DigitalInput(int id,
+				 int parentId,
+				 int port,
+				 bool reverse,
+				 unsigned long debounceTimeMillis);
+	
+	void tick(unsigned long passedTime);
+	void setInputValueFromHardware(bool value);
+	
+/*ToDo*/	bool currentValue();
+/*ToDo*/	bool currentValueTime();
+/*ToDo*/	bool lastValue();
+/*ToDo*/	bool lastValueTime();
+	
+private:
+	int _id;
+	int _parentId;
+	int _port;
+	bool _reverse;
+	
+	bool _currentHardwareValue;
+	bool _lastHardwareValue;
+	bool _currentValue;
+	bool _lastValue;
+	unsigned long _lastValueDuration;
+	
+	TimeElement* _debounceTimer;
+	TimeElement* _currentValueTimer;
 };
 
 
 
 
-void DigitalInput::DigitalInput(int id, 
-                     int parentId, 
-                     int portNoOnParentHardware, 
-                     bool reverseValue){
-    
+void DigitalInput::DigitalInput(String name,
+								int id,
+								int parentId,
+								int port,
+								bool reverse,
+								unsigned long debounceTimeMillis){
+	// store parameters
+	_name = name
+	_id = id;
+	_parentId = parentId;
+	_port = port;
+	_reverse = reverse;
+	
+	// initialize Timers
+	_debounceTimer = new TimeElement(debounceTimeMillis);
+	_currentValueTimer = new TimeElement();
 }
 
-void DigitalInput::tick()
-void setInputValueFromHardware(bool value) {
-    
+void DigitalInput::tick(unsigned long passedTime) {
+	_debounceTimer->tick(passedTime);
+}
+
+void DigitalInput::setInputValueFromHardware(bool value) {
+	_lastHardwareValue = _currentHardwareValue;
+	_currentHardwareValue = value;
+	
+	
+	
+	if (_currentHardwareValue != _currentValue) {
+		// input from Hardware is different to currentValue
+		if (_currentHardwareValue != _lastHardwareValue) {
+			// reset debounceTimer
+			_debounceTimer.reset();
+		} else {
+			// seemes to be stable signal so check if debounceTimer is finished
+			if(_debounceTimer->isFinished()) {
+				//timer finished so pass value to "real" value store old and resert valueTimer
+				
+				_lastValue = _currentValue;
+				_lastValueDuration = _currentValueTimer->passedTime();
+				_currentValue = _currentHardwareValue;
+				_currentValueTimer.reset();
+			} else {
+				// nothing to do here; still wait for next loop's setting hardwareValue
+			}
+		}
+	} else {
+		// input equals hardware so no debounce needed
+		_debounceTimer.pause();
+	}
 }
--- Node/Base/TimeElement.h
+++ Node/Base/TimeElement.h
@@ -1,52 +1,75 @@
 #include Constants.h
 
 class TimeElement {
-    public:
-        TimeElement(unsigned long initTime,
-                    unsigned long doneTime,
-                    TimeElement::Mode mode
-                   );
-        void tick();
-    
-    bool isFinished();
+public:
+	TimeElement();
 	
-    
-    
-        
-    
+	TimeElement(unsigned long endTime);
 	
-    enum static Mode {
-        INCREMENTAL, // time increases on every Tick
-        DECREMENTAL  // time decreases on every Tick
-    };
-    
-    private:
-        unsigned long _initTime;
-        unsigned long _doneTime;
-        unsigned long _passedTime;
-        TimeElement::Mode _mode;
-        
-            
+	TimeElement(unsigned long startTime,
+				unsigned long endTime,
+				TimeElement::Mode mode
+				);
+	
+	
+	void tick();
+	
+	bool isFinished();
+	
+	int passedRatio();
+	
+	unsigned long passedTime();
+	
+	void pause();
+	
+	void continue();
+	
+	void reset();
+	
+	
+	
+	enum static Mode {
+		INCREMENTAL, // time increases on every Tick
+		DECREMENTAL  // time decreases on every Tick
+	};
+	
+private:
+	unsigned long _startTime;
+	unsigned long _endTime;
+	unsigned long _passedTime;
+	bool _isPaused;
+	TimeElement::Mode _mode;
+	
+	
 };
+void TimeElement::TimeElement() {
+	TimeElement(0ul, UNSIGNED_LONG_MAX, INCREMENTAL);
+}
 
+void TimeElement::TimeElement(unsigned long endTime) {
+	TimeElement(0ul, endTime, INCREMENTAL);
+}
 
-
-
-void TimeElement::TimeElement(unsigned long initTime,
-                              unsigned long doneTime,
-                              TimeElement::Mode mode){
-    _initTime = initTime;
-	_doneTime = doneTime;
+void TimeElement::TimeElement(unsigned long startTime,
+							  unsigned long endTime,
+							  TimeElement::Mode mode){
+	_initTime = initTime;
+	_startTime = startTime;
 	_mode = mode;
 	_passedTime = _initTime;
+	_isPaused = false;
 }
 
 
-void TimeElement::tick(){
+void TimeElement::tick(unsigned long passedTime){
+	if (this->isFinished() || _isPaused){
+		return;
+	}
 	//TODO: gather loopDoration time
-    unsigned long loopDurationTime = 10;
+	unsigned long loopDurationTime = passedTime;
 	
 	if (_mode == INCREMENTAL) {
+		
 		var newPassedTime = _passedTime+=loopDurationTime;
 		if (newPassedTime >= _passedTime) {
 			// set _passedTime
@@ -68,3 +91,70 @@ void TimeElement::tick(){
 		}
 	}
 }
+
+bool TimeElement::isFinished(){
+	if (_mode == INCREMENTAL) {
+		if (_passedTime >= _endTime) {
+			return true;
+		}
+	}
+	
+	if (_mode == DECREMENTAL) {
+		if (_passedTime <= _endTime) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+unsigned long TimeElement::passedTime() {
+	unsigned long passed = 0;
+	if (_mode == INCREMENTAL) {
+		passed = _passedTime - _startTime;
+	}
+	
+	if (_mode == DECREMENTAL) {
+		complete = _startTime - _endTime;
+		passed = _passedTime - _endTime;
+		passed = complete - passed;
+	}
+}
+
+int TimeElement::passedRatio(){
+	unsigned long complete = 100
+	unsigned long passed = 0;
+	
+	if (_mode == INCREMENTAL) {
+		complete = _endTime - _startTime;
+		passed = _passedTime - _startTime;
+	}
+	
+	if (_mode == DECREMENTAL) {
+		complete = _startTime - _endTime;
+		passed = _passedTime - _endTime;
+		passed = complete - passed;
+	}
+	
+	unsigned long result = (1000ul * passed) / complete;
+	if (result > 1000) {
+		// someThing went wrong
+		return 0;
+	} else {
+		return (int)result;
+	}
+}
+
+void TimeElement::reset() {
+	_passedTime = _initTime;
+	_isPaused = false;
+}
+
+void TimeElement::pause() {
+	_isPaused=true;
+}
+
+void TimeElement::continue() {
+	_isPaused=false;
+}
+
